7b0866967f59c9c7d73e67c541d6771e5ba93c08
Akiz-Ivanov <ivanovicandreja@gmail.com>
2025-05-14 15:37:22 +0200

    fix: comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header"
 import Rating from "./components/Rating"
-import BuyerCard from "./components/BuyerCard"
-
+import TestimonialsCard from "./components/TestimonialsCard"
 import colton from './assets/images/image-colton.jpg'
 import irene from './assets/images/image-irene.jpg'
 import anne from './assets/images/image-anne.jpg'
@@ -15,29 +14,33 @@ function App() {
       max-w-[71.25rem] text-body place-items-center
       gap-40-72"
       >
-        
+      
+      {/* Header section */}
         <Header />
 
+      {/* Ratings section with stars */}
         <aside className="flex flex-col gap-4 w-full items-start md:gap-6">
           <Rating rating={5} reviewer="Reviews" />
           <Rating rating={5} reviewer="Report Guru" className="lg:ml-8-40" />
           <Rating rating={5} reviewer="BestTech" className="lg:ml-16-80"/>
         </aside>
 
+      {/* Testimonials section */}
         <div className="md:col-span-2 grid md:grid-cols-3 gap-6" >
-          <BuyerCard name="Colton Smith" img={colton} >
+          
+          <TestimonialsCard name="Colton Smith" img={colton} >
             We needed the same printed design as the one we had ordered a week prior.
             Not only did they find the original order, but we also received it in time.
             Excellent!
-          </BuyerCard>
-          <BuyerCard name="Irene Roberts" img={irene} className="md:mt-4">
+          </TestimonialsCard>
+          <TestimonialsCard name="Irene Roberts" img={irene} className="md:mt-4">
             Customer service is always excellent and very quick turn around. Completely
             delighted with the simplicity of the purchase and the speed of delivery.
-          </BuyerCard>
-          <BuyerCard name="Anne Wallace" img={anne} className="md:mt-8">
+          </TestimonialsCard>
+          <TestimonialsCard name="Anne Wallace" img={anne} className="md:mt-8">
             Put an order with this company and can only praise them for the very high
             standard. Will definitely use them again and recommend them to everyone!
-          </BuyerCard>
+          </TestimonialsCard>
 
         </div>
 
--- src/components/TestimonialsCard.jsx
+++ src/components/TestimonialsCard.jsx
@@ -1,10 +1,11 @@
-export default function BuyerCard({ name, children, img, className = "" }) {
+export default function TestimonialsCard({ name, children, img, className = "" }) {
     return (
         <article
             className={`bg-primary-magenta px-8 py-8 rounded-md text-neutral-light-magenta
           flex flex-col gap-[clamp(1rem,1.5vw,1.5rem)] w-full text-paragraph h-full
           md:pt-[clamp(2rem,2.5vw,2.5rem)] ${className} md:pb-2`}
         >
+            
             <header className="flex items-center gap-6">
                 <img src={img} alt={name} className="w-10 h-10 rounded-full" />
                 <div>
@@ -15,6 +16,7 @@ export default function BuyerCard({ name, children, img, className = "" }) {
             <blockquote>
                 <p className="text-[1rem] font-medium">&ldquo; {children} &rdquo;</p>
             </blockquote>
+
         </article>
-    );
+    )
   }
